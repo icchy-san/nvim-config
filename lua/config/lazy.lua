19c3fe8494db86a8a141a65a478f7417a6fe80f4
icchy-san <icchy.tech@gmail.com>
2024-04-06 09:11:47 +0900

    feat: added language support for go and some updates

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,9 +14,9 @@ require("lazy").setup({
       colorscheme = "solarized-osaka",
     } },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- import/override with your plugins
     { import = "plugins" },
   },
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "astro",
+        "cmake",
+        "cpp",
+        "css",
+        "gitignore",
+        "go",
+        "graphql",
+        "http",
+        "rust",
+        "sql",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+
+      -- MDX
+      vim.filetype.add({
+        extenstion = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
+  },
+}
